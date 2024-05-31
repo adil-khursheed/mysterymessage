82a2d205f16e60339dc4db7b4408ca0acad285e5
adil-khursheed <adilkhursheed60@gmail.com>
2024-05-31 20:03:36 +0530

    implemented signIn, signUp and verifyCode Pages with shadcn UI components

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,23 +1,137 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 
-export default function Page() {
-  const { data: session } = useSession();
+import { signInSchema } from "@/schemas/signInSchema";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import { signIn } from "next-auth/react";
 
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user.name}</p>
-        <button onClick={() => signOut()}>Sign Out</button>
-      </>
-    );
-  }
+const SignInForm = () => {
+  const [isSigningIn, setIsSigningIn] = useState(false);
 
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSigningIn(true);
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    console.log(result);
+
+    if (result?.error) {
+      if (result.error === "CredentialsSignin") {
+        toast({
+          title: "Login Failed",
+          description: "Incorrect credentials",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+
+    setIsSigningIn(false);
+  };
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign In</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back To True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" disabled={isSigningIn}>
+              {isSigningIn ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Signing In
+                </>
+              ) : (
+                <>Sign In</>
+              )}
+            </Button>
+          </form>
+        </Form>
+
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default SignInForm;
---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,65 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel, { Message } from "@/model/user.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found!",
+        },
+        { status: 404 }
+      );
+    }
+
+    //is user accepting the messages
+    if (!user.isAcceptingMessages) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not accepting the messages!",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const newMessage = {
+      content,
+      createdAt: new Date(),
+    };
+
+    user.messages.push(newMessage as Message);
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message sent successfully!",
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.log("Failed to send message: ", error);
+
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to send message",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
